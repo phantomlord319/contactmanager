29bc34f11e85c1ddaf3777c97dbd8771a0b8619a
phantomlord319 <ankit.dubey319@gmail.com>
2020-01-05 01:01:04 +0530

    Updates

---
+++ precache-manifest.6e458e3ca12fc5f0ba783939f054d234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35bd3f36c67d5dc11efe4804172962af",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "1ae5f6a8460708434940",
+    "url": "/contactmanager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "fa2059cac588e7498a80",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "1ae5f6a8460708434940",
+    "url": "/contactmanager/static/js/2.d9fbd005.chunk.js"
+  },
+  {
+    "revision": "fa2059cac588e7498a80",
+    "url": "/contactmanager/static/js/main.108796c0.chunk.js"
+  },
+  {
+    "revision": "9e7d9bf971973a9a8140",
+    "url": "/contactmanager/static/js/runtime-main.756906da.js"
+  }
+]);
